d30ac699476e0c2bb41b874010b965332bbab884
USERMIC-9H64N6C\BIFF2020 <dnjswnsthd@naver.com>
2022-07-19 16:31:57 +0900

    Add java06_Condition

---
+++ java06_Condition/src/com/edu/condition/test/GradeIfTest4.java
@@ -0,0 +1,22 @@
+package com.edu.condition.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GradeIfTest4 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("점수를 입력해주세요.");
+		int score = Integer.parseInt(br.readLine());
+		String grade = ""; 
+		switch(score){
+			case 90: System.out.println("A"); break;
+			case 80: System.out.println("B"); break;
+			case 70: System.out.println("C"); break;
+			case 60: System.out.println("D"); break;
+			default : System.out.println("f");
+		}
+		
+	}
+}
