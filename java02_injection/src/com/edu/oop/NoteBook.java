72cd4dff6698cd556b1cfda2adc76e2e72dba72e
USERMIC-9H64N6C\BIFF2020 <dnjswnsthd@naver.com>
2022-07-19 09:37:04 +0900

    Add

---
+++ java02_injection/src/com/edu/oop/NoteBook.java
@@ -0,0 +1,29 @@
+package com.edu.oop;
+/*
+ * 어제 핵심 이슈
+ * object Class	  			| 		~ Test (Test 클래스, 실행 클래스)
+ * main() 이 없다 ---> 컾마일의 대상			main() 이 있다 ---> 실행의 대상
+ * 									1. 객체 생성 == 클래스의 멤버를 메모리에 올린다.
+ * 									2. 접근 
+ * 										필드 -> 값 할당
+ * 										메소드 -> 호출
+ * 구성요소
+ * filed(멤버 변수) : 값이 저장되는 공간
+ * method : 기능 
+ *  --------------------------------------------------------------------
+ *  NoteBook 
+*/
+
+public class NoteBook {
+	// 1. 필드 선언
+	public String brand;
+	public int price;
+	public int serialNumber;
+	
+	// 2. 메소드
+	public void printNoteBookInfo() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("NotebootInfo :: " + brand + ", " + price + ", " + serialNumber + "\n");
+		System.out.println(sb);
+	}
+}
---
+++ java02_injection/src/com/edu/oop/test/NoteBookTest.java
@@ -0,0 +1,16 @@
+package com.edu.oop.test;
+
+import com.edu.oop.NoteBook;
+
+public class NoteBookTest {
+	public static void main(String[] args) {
+		/*
+		 * 1. 객체 생성
+		 * 2. 값할당 | 메소드 호출
+		 */
+		NoteBook nb = new NoteBook();
+		nb.brand = "samsung";
+		nb.price = 1000000;
+		nb.serialNumber = 1000;
+	}
+}
